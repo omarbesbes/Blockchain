7cd1879f254505c8b0d725278029c0f250b0d061
Houssem Guermazi <houssem.guermazi@student-cs.fr>
2025-04-18 05:44:50 +0800

    add not found page

---
+++ app/supply-chain-dapp/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+// src/pages/NotFound.jsx
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>404: Page Not Found</h2>
+      <p>Oops! The page you’re looking for doesn’t exist.</p>
+    </div>
+  );
+}
